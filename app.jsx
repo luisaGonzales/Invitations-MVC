1d3c6fd58a244912f50738a14ee74899afac5ada
luisaGonzales <luisagc711@gmail.com>
2017-10-04 10:49:18 -0500

    Función de Submit Lista

---
+++ app.jsx
@@ -0,0 +1,103 @@
+{/* <div class="wrapper">
+      <header>
+        <h1>RSVP</h1>
+        <p> Registration App </p>
+        <form id="registrar">
+          <input type="text" name="name" placeholder="Invite Someone">
+          <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
+      </header>
+      
+      <div class="main">	
+        <h2>Invitees</h2>
+        <ul id="invitedList"></ul>	
+      </div>
+    </div> */}
+
+
+class Model {
+	constructor(){
+		this.render = undefined;
+		this.inputValue = "";
+		this.invitees = [];
+	}
+	suscribe(render){
+		this.render = render;
+	}
+	inform(render){
+		this.render();
+	}
+	addSomeone(someone){
+		this.invitees.push({
+			name: someone,
+			id: Utils.uuid()
+		})
+		console.log(someone);
+		console.log(this.invitees);
+		this.inputValue = "";
+		this.inform();
+	}
+	onChange(e){
+		this.inputValue = e.target.value;
+		this.inform();
+	}
+}
+
+const Header = (props) => {
+	return (
+		<div>
+			<header>
+				<h1>RSVP</h1>
+				<p> Registration App </p>
+				<form id="registrar" onSubmit={e => {
+					e.preventDefault();
+					props.model.addSomeone(props.model.inputValue);
+					props.model.inform();
+				}}>
+					<input type="text" name="name" placeholder="Invite Someone" onChange={(e) => {props.model.onChange(e)}} value={props.model.inputValue}/>
+					<button type="submit" name="submit" value="submit">Submit</button>
+				</form>
+      		</header>
+		</div>
+	);
+}
+
+const List = (props) => {
+	const allPeople = props.model.invitees.map((someone, index) => {
+		return (
+			<li key={someone.id}>
+				{someone.name}
+				<label> Confirmed 
+					<input type="checkbox"/>
+				</label>
+				<button>Remove</button>
+			</li>
+		);
+	});
+	return (
+		<div>
+			<div className="main">	
+				<h2>Invitees</h2>
+				<ul id="invitedList">{allPeople}</ul>	
+      		</div>
+		</div>
+	);
+}
+
+let model = new Model();
+
+const Application = ({model}) => {
+   return (
+	<div className="wrapper">
+		<Header model={model}/>
+		<List model={model}/>
+	</div>      
+   );
+}
+
+let render = () => {
+	ReactDOM.render(<Application model={model}/>, document.getElementById('container'));
+};
+
+model.suscribe(render);
+render();
